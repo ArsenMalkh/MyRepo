4f7477786da449f0052ec24eaa0eb01622f5b5ef
ArsenMalkh <61829741+ArsenMalkh@users.noreply.github.com>
2021-01-30 05:27:35 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Early.h"
+
+
+int main()
+{
+	char left;
+	int n;
+	std::string right;
+	std::vector<Grammar> grammars;
+	std::cin>>n;
+	while(n--)
+	{
+		std::cin>>left;
+		std::cin>>right;
+		grammars.emplace_back(Grammar(left, right));	
+	}
+	std::string word;
+	std::cin>> word;
+	EarleyAnalyse parser(grammars, 'S');
+	if(parser.CompriseSymbol(word)){
+		std::cout<<"YES"<<std::endl;
+	} else {
+		std::cout<<"NO"<<std::endl;
+	}
+	return 0;
+}
